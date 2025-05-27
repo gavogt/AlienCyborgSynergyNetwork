333d49a2e8e1e59746f40da3b08e2ac45f3384f3
Gabriel <gvogt@asu.edu>
2025-05-27 04:23:22 -0700

    Added a model for Register and a DBContextServices class

---
+++ AlienCyborgSynergyNetwork/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCyborgSynergyNetwork
+{
+    public class RegisterModel
+    {
+        public string Email { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+        public string ConfirmPassword { get; set; } = String.Empty;
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public int Age { get; set; } = 0;
+    }
+}
