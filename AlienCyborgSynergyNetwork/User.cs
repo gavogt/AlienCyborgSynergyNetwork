6bdd8211a331531c30a4d2d9ffa8f2dd3ad015c1
Gabriel <gvogt@asu.edu>
2025-05-27 01:45:30 -0700

    Created a User class for the login

---
+++ AlienCyborgSynergyNetwork/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCyborgSynergyNetwork
+{
+    internal class User
+    {
+        [Key]
+        private Guid Guid { get; set; }
+        private string Email { get; set; }
+        private string Password { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private int Age { get; set; }
+
+        public User(string Email, string Password, string FirstName, string LastName, int Age)
+        {
+            this.Guid = Guid.NewGuid();
+            this.Email = Email;
+            this.Password = Password;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Age = Age;
+
+        }
+    }
+}
