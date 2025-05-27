6c0791b47e0749f37171f31f92c9e9d0d2648474
Gabriel <gvogt@asu.edu>
2025-05-27 02:46:56 -0700

    Added a DBContext

---
+++ AlienCyborgSynergyNetwork/SynergyDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlienCyborgSynergyNetwork
+{
+    internal class SynergyDBContext : DbContext
+    {
+
+        public SynergyDBContext(DbContextOptions<SynergyDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<SessionState> Sessions { get; set; }
+
+    }
+}
