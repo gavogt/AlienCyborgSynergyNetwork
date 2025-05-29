830056ef8ed2cb9592b7b9f2391c63b3025c53ff
Gabriel <gvogt@asu.edu>
2025-05-28 18:14:20 -0700

    Created a DBContext for CyborgSession

---
+++ AlienCyborgSynergyNetwork/CyborgSessionDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCyborgSynergyNetwork
+{
+    public class CyborgSessionDBContext : DbContext
+    {
+        public DbSet<CyborgSession> CyborgSessions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename={Path.Combine(FileSystem.AppDataDirectory, "cyborg.db")}");
+        }
+    }
+}
