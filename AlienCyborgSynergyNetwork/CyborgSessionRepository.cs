728cfe7e6c7fa6d62d8cf391ad7cff162dbe536b
Gabriel <gvogt@asu.edu>
2025-05-28 18:52:50 -0700

    feat(cyborg-session): implement CRUD operations
    - Add AddAsync, GetByIdAsync, ListAsync, Update, Delete methods in CyborgSessionRepository
    - Leverage EF Core’s DbContext for tracking/in-memory operations
    - Return Task-based signatures for async workflows

--- AlienCyborgSynergyNetwork/CyborgSessionRepository.cs
+++ AlienCyborgSynergyNetwork/CyborgSessionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace AlienCyborgSynergyNetwork
 {
-    internal class CyborgSessionRepository
+    internal class CyborgSessionRepository : ICyborgSessionRepository
     {
+        private readonly CyborgSessionDBContext _context;
+
+        public CyborgSessionRepository(CyborgSessionDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(CyborgSession session)
+        {
+            await _context.CyborgSessions.AddAsync(session).AsTask();
+        }
+
+        public void Delete(CyborgSession session)
+        {
+            _context.CyborgSessions.Remove(session);
+        }
+
+        public Task<CyborgSession?> GetByIdAsync(Guid id)
+        {
+           return _context.CyborgSessions.FindAsync(id).AsTask();
+        }
+
+        public Task<List<CyborgSession>> ListAsync()
+        {
+            return _context.CyborgSessions.ToListAsync();
+        }
+
+        public void Update(CyborgSession session)
+        {
+            _context.CyborgSessions.Update(session);
+        }
     }
 }
