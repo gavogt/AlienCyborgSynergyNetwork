48b4d547ff658e53b02234752971c056c9ff2c77
Gabriel <gvogt@asu.edu>
2025-06-09 11:18:12 -0700

    feat(dbcontext): setup entity keys and navigation for CyborgSession and SensorReading

--- AlienCyborgSynergyNetwork/CyborgSessionDBContext.cs
+++ AlienCyborgSynergyNetwork/CyborgSessionDBContext.cs
@@ -10,10 +10,22 @@ namespace AlienCyborgSynergyNetwork
     public class CyborgSessionDBContext : DbContext
     {
         public DbSet<CyborgSession> CyborgSessions { get; set; }
+        public DbSet<SensorReading> Readings { get; set; }
 
         public CyborgSessionDBContext(DbContextOptions<CyborgSessionDBContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CyborgSession>().HasKey(s => s.Id);
+            modelBuilder.Entity<SensorReading>()
+                .HasKey(r => r.ID);
+            modelBuilder.Entity<SensorReading>()
+                .HasOne(s => s.Session)
+                .WithMany(s => s.SensorReadings)
+                .HasForeignKey(r => r.SessionId);
         }
     }
 }
