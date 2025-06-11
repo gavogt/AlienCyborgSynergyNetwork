5c12e3137e148a430fe26c84e790ecb1345d1698
Gabriel <gvogt@asu.edu>
2025-06-11 07:47:01 -0700

    feat(firmware): scaffold FirmwareDbContext and FirmwareRepository

---
+++ AlienCyborgSynergyNetwork.Shared/FirmwareRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlienCyborgSynergyNetwork.Shared
+{
+    public class FirmwareRepository : IFirmwareRepository
+    {
+        private readonly FirmwareDBContext _context;
+
+        public FirmwareRepository(FirmwareDBContext context) => _context = context;
+
+        public Task AddAsync(FirmwareImage image)
+        {
+            return _context.FirmwareImages.AddAsync(image).AsTask();
+        }
+
+        public Task<FirmwareImage?> GetLastestAsync()
+        {
+            return _context.FirmwareImages
+                .OrderByDescending(f => f.Created)
+                .FirstOrDefaultAsync();
+
+        }
+    }
+}
