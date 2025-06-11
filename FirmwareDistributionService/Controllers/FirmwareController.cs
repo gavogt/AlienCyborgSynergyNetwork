00229de264ebe2a0727f5e2e7c8acefe7fdff0c0
Gabriel <gvogt@asu.edu>
2025-06-11 04:10:10 -0700

    Add FirmwareController with GET /api/firmware/latest to serve firmware.bin. Rename files.

---
+++ FirmwareDistributionService/Controllers/FirmwareController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirmwareDistributionService.Controllers
+{
+    [ApiController]
+    [Route("api/firmware")]
+    public class FirmwareController : ControllerBase
+    {
+        [HttpGet("latest")]
+        public IActionResult GetLatest()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "firmware.bin");
+
+            if (!System.IO.File.Exists(path))
+            {
+                return NotFound("Firmware file not found.");
+            }
+
+            return PhysicalFile(path, "application/octet-stream", "firmware.bin");
+
+        }
+    }
+}
