2c8ae2f70626498e1abff8d17c6d49958680ae70
Gabriel <gvogt@asu.edu>
2025-06-18 05:58:44 -0700

    feat(firmware): add SignCommand and PushCommand for update pipeline

---
+++ AlienCyborgSynergyNetwork.Shared/PushCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCyborgSynergyNetwork.Shared
+{
+    public class PushCommand : ICommand
+    {
+        public async Task ExecuteAsync(FirmwareJob job, IProgress<int> progress)
+        {
+            await Task.Delay(500);
+            progress.Report(75);
+            await Task.Delay(500);
+            progress.Report(100);
+        }
+    }
+}
---
+++ AlienCyborgSynergyNetwork.Shared/SignCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCyborgSynergyNetwork.Shared
+{
+    public class SignCommand : ICommand
+    {
+        public Task ExecuteAsync(FirmwareJob job, IProgress<int> progress)
+        {
+            progress.Report(25);
+            return Task.CompletedTask;
+        }
+    }
+}
