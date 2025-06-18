494921943c124654e5971ee186d041680a48ca7e
Gabriel <gvogt@asu.edu>
2025-06-18 07:29:28 -0700

    feat(rabbitmq): add JobPublisher.InitAsync to open firmware_jobs queue. Move commands and publisher to FirmwareDistributionService

---
+++ FirmwareDistributionService/JobPublisher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlienCyborgSynergyNetwork.Shared;
+using RabbitMQ.Client;
+
+namespace FirmwareDistributionService
+{
+    public class JobPublisher
+    {
+        // RabbitMQ connection and channel
+        private IChannel? _channel;
+        private IConnection? _connection;
+
+        // RabbitMQ connection factory
+        private readonly ConnectionFactory _factory;
+
+        public JobPublisher()
+        {
+            // Connect to localhost
+            _factory = new ConnectionFactory { HostName = "localhost" };
+
+        }
+
+        public async Task InitAsync()
+        {
+            // Open Connection
+            _connection = await _factory.CreateConnectionAsync();
+
+            // Open Channel
+            _channel = await _connection.CreateChannelAsync();
+
+            // Declare a durable queue for firmware jobs
+            await _channel.QueueDeclareAsync(queue: "firmware_jobs",
+                                 durable: true, // Survive server restart
+                                 exclusive: false, // more than one consumer can use it
+                                 autoDelete: false, // don't delete it when last consumer leaves
+                                 arguments: null); // no fancy options
+
+        }
+    }
+}
--- FirmwareDistributionService/SignCommand.cs
+++ FirmwareDistributionService/SignCommand.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AlienCyborgSynergyNetwork.Shared;
 
-namespace AlienCyborgSynergyNetwork.Shared
+namespace FirmwareDistributionService
 {
     public class SignCommand : ICommand
     {
