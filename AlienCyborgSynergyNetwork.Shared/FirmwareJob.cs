48c5458b83d6c3525a6dc25f095f1f5522882540
Gabriel <gvogt@asu.edu>
2025-06-18 05:46:06 -0700

    Add a FirmwareJob record with version parameter

---
+++ AlienCyborgSynergyNetwork.Shared/FirmwareJob.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCyborgSynergyNetwork.Shared
+{
+    public record FirmwareJob(string version);
+}
