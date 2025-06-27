e9d2cafefdd341e6a52f688ad160df2a60098f85
Gabriel <gvogt@asu.edu>
2025-06-27 11:29:22 -0700

    feat(analytics): add Analytics.razor and helper data models
    - Introduce `HeatMapDataItem` and `DailyDataItem` classes for chart binding
    - Create `Analytics.razor` with scaffolding for heatmap and line chart integration

---
+++ AlienCyborgSynergyNetwork.Shared/DailyDataItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCyborgSynergyNetwork.Shared
+{
+    public class DailyDataItem
+    {
+        public string Date { get; set; } = "";
+        public double Stability { get; set; }
+        public double Efficiency { get; set; }
+        public double Cohesion { get; set; }
+    }
+}
---
+++ AlienCyborgSynergyNetwork.Shared/HeatMapDataItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlienCyborgSynergyNetwork.Shared
+{
+    public class HeatMapDataItem
+    {
+        public string XLabel { get; set; } = "";
+        public int YIndex { get; set; }
+        public int Value { get; set; }
+    }
+}
